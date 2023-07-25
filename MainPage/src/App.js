dab8633a1030e4e2be78e3be11413234efc92c88
이진형 <wlsgud4084@naver.com>
2023-07-25 15:37:34 +0900

    feat: Add various components
    거울 UI에 필요한 다양한 Component들을 추가했습니다.

--- MainPage/src/App.js
+++ MainPage/src/App.js
@@ -1,14 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Weather from './components/Weather'
-import CurrentDate from './components/CurrentDate'
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+
+import Weather from './components/Weather';
+import CurrentDate from './components/CurrentDate';
+import ToDoList from './components/ToDoList';
+import VideoMessage from './components/VideoMessage';
+import FavoriteNews from './components/FavoriteNews';
+import Character from './components/Character';
 
 function App() {
   return (
-    <div>
-      <Weather></Weather>
-      <CurrentDate></CurrentDate>
+    <div className="App">
+      <Weather className="Weather"></Weather>
+      <CurrentDate className="CurrentDate"></CurrentDate>
+      <ToDoList></ToDoList>
+      <VideoMessage></VideoMessage>
+      <FavoriteNews></FavoriteNews>
+      <Character></Character>
     </div>
   );
 }
