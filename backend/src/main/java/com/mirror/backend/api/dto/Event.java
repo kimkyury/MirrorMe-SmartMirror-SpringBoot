4fdccb5e2d19a05c6ef0439c8590470798df1652
황주원 <woneee99@naver.com>
2023-07-25 17:58:19 +0900

    feat: calendar response & project response sturcture

--- backend/src/main/java/com/mirror/backend/api/dto/Event.java
+++ backend/src/main/java/com/mirror/backend/api/dto/Event.java
@@ -1,121 +1,167 @@
 package com.mirror.backend.api.dto;
 
 import ch.qos.logback.core.status.Status;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.api.services.calendar.model.ConferenceSolution;
 import com.google.api.services.calendar.model.ConferenceSolutionKey;
 import com.google.api.services.calendar.model.EntryPoint;
 import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Event {
-    private @JsonProperty("kind") String kind;
-    private @JsonProperty("etag") String etag;
-    private @JsonProperty("summary") String summary;
-    private @JsonProperty("updated") String updated;
-    private @JsonProperty("timeZone") String timeZone;
-    private @JsonProperty("accessRole") String accessRole;
+    @JsonProperty("kind")
+    private String kind;
+    @JsonProperty("etag")
+    private String etag;
+    @JsonProperty("summary")
+    private String summary;
+    @JsonProperty("updated")
+    private String updated;
+    @JsonProperty("timeZone")
+    private String timeZone;
+    @JsonProperty("accessRole")
+    private String accessRole;
+    @JsonProperty("defaultReminders")
+    private List<Reminder> defaultReminders;
+    @JsonProperty("nextSyncToken")
+    private String nextSyncToken;
+    @JsonProperty("items")
+    private List<Item> items;
 
-    private @JsonProperty("defaultReminders") List<Reminder> defaultReminders;
-    private @JsonProperty("nextSyncToken") String nextSyncToken;
-    private @JsonProperty("items") List<Item> items;
+    @Override
+    public String toString() {
+        return "Event{" +
+                "kind='" + kind + '\'' +
+                ", etag='" + etag + '\'' +
+                ", summary='" + summary + '\'' +
+                ", updated='" + updated + '\'' +
+                ", timeZone='" + timeZone + '\'' +
+                ", accessRole='" + accessRole + '\'' +
+                ", defaultReminders=" + defaultReminders +
+                ", nextSyncToken='" + nextSyncToken + '\'' +
+                ", items=" + items +
+                '}';
+    }
 
-    public Event() {}
     @Builder
-    public Event(String kind, String etag, String summary, String updated, String timeZone, String accessRole, String nextSyncToken, List<Item> items) {
+    public Event(String kind, String etag, String summary, String updated, String timeZone, String accessRole, List<Reminder> defaultReminders, String nextSyncToken, List<Item> items) {
         this.kind = kind;
         this.etag = etag;
         this.summary = summary;
         this.updated = updated;
         this.timeZone = timeZone;
         this.accessRole = accessRole;
+        this.defaultReminders = defaultReminders;
         this.nextSyncToken = nextSyncToken;
         this.items = items;
     }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Reminder {
-        private @JsonProperty("method") String method;
-        private @JsonProperty("minutes") int minutes;
+        @JsonProperty("method")
+        private String method;
+        @JsonProperty("minutes")
+        private int minutes;
     }
+
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Item {
-        private @JsonProperty("kind") String kind;
-        private @JsonProperty("etag") String etag;
-        private @JsonProperty("id") String id;
-        private @JsonProperty("status") String status;
-        private @JsonProperty("htmlLink") String htmlLink;
-        private @JsonProperty("created") String created;
-        private @JsonProperty("updated") String updated;
-        private @JsonProperty("summary") String summary;
-        private @JsonProperty("creator") Creator creator;
-        private @JsonProperty("organizer") Organizer organizer;
-        private @JsonProperty("start") StartEndDateTime start;
-        private @JsonProperty("end") StartEndDateTime end;
-        private @JsonProperty("iCalUID") String iCalUID;
-        private @JsonProperty("sequence") int sequence;
-        private @JsonProperty("attendees") List<Attendee> attendees;
-        private @JsonProperty("hangoutLink") String hangoutLink;
-        private @JsonProperty("eventType") String eventType;
-
-        public Item() {}
-        @Builder
-        public Item(String kind, String etag, String id, String status, String htmlLink, String created, String updated, String summary, Creator creator, Organizer organizer, StartEndDateTime start, StartEndDateTime end, String iCalUID, int sequence, List<Attendee> attendees, String hangoutLink, String eventType) {
-            this.kind = kind;
-            this.etag = etag;
-            this.id = id;
-            this.status = status;
-            this.htmlLink = htmlLink;
-            this.created = created;
-            this.updated = updated;
-            this.summary = summary;
-            this.creator = creator;
-            this.organizer = organizer;
-            this.start = start;
-            this.end = end;
-            this.iCalUID = iCalUID;
-            this.sequence = sequence;
-            this.attendees = attendees;
-            this.hangoutLink = hangoutLink;
-            this.eventType = eventType;
-        }
+        @JsonProperty("kind")
+        private String kind;
+        @JsonProperty("etag")
+        private String etag;
+        @JsonProperty("id")
+        private String id;
+        @JsonProperty("status")
+        private String status;
+        @JsonProperty("htmlLink")
+        private String htmlLink;
+        @JsonProperty("created")
+        private String created;
+        @JsonProperty("updated")
+        private String updated;
+        @JsonProperty("summary")
+        private String summary;
+        @JsonProperty("creator")
+        private Creator creator;
+        @JsonProperty("organizer")
+        private Organizer organizer;
+        @JsonProperty("start")
+        private StartEndDateTime start;
+        @JsonProperty("end")
+        private StartEndDateTime end;
+        @JsonProperty("iCalUID")
+        private String iCalUID;
+        @JsonProperty("sequence")
+        private int sequence;
+        @JsonProperty("attendees")
+        private List<Attendee> attendees;
+        @JsonProperty("hangoutLink")
+        private String hangoutLink;
+        @JsonProperty("eventType")
+        private String eventType;
     }
 
-    @NoArgsConstructor
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Organizer {
-        private @JsonProperty("email") String email;
+        @JsonProperty("email")
+        private String email;
     }
 
-    @NoArgsConstructor
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class StartEndDateTime {
-        private @JsonProperty("dateTime") String dateTime;
-        private @JsonProperty("timeZone") String timeZone;
+        @JsonProperty("dateTime")
+        private String dateTime;
+        @JsonProperty("timeZone")
+        private String timeZone;
     }
-
-    @NoArgsConstructor
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Attendee {
-        private @JsonProperty("email") String email;
-        private @JsonProperty("organizer") boolean organizer;
-        private @JsonProperty("responseStatus") String responseStatus;
+        @JsonProperty("email")
+        private String email;
+        @JsonProperty("organizer")
+        private boolean organizer;
+        @JsonProperty("self")
+        private boolean self;
+        @JsonProperty("responseStatus")
+        private String responseStatus;
     }
-
-    @NoArgsConstructor
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class ConferenceData {
-        private @JsonProperty("createRequest") CreateRequest createRequest;
-        private @JsonProperty("entryPoints") List<EntryPoint> entryPoints;
-        private @JsonProperty("conferenceSolution") ConferenceSolution conferenceSolution;
-        private @JsonProperty("conferenceId") String conferenceId;
+        @JsonProperty("createRequest")
+        private CreateRequest createRequest;
+        @JsonProperty("entryPoints")
+        private List<EntryPoint> entryPoints;
+        @JsonProperty("conferenceSolution")
+        private ConferenceSolution conferenceSolution;
+        @JsonProperty("conferenceId")
+        private String conferenceId;
     }
-
-    @NoArgsConstructor
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class CreateRequest {
-        private @JsonProperty("requestId") String requestId;
-        private @JsonProperty("conferenceSolutionKey") ConferenceSolutionKey conferenceSolutionKey;
-        private @JsonProperty("status") Status status;
+        @JsonProperty("requestId")
+        private String requestId;
+        @JsonProperty("conferenceSolutionKey")
+        private ConferenceSolutionKey conferenceSolutionKey;
+        @JsonProperty("status")
+        private Status status;
     }
-
-    @NoArgsConstructor
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Creator {
-        private @JsonProperty("email") String email;
+        @JsonProperty("email")
+        private String email;
     }
 
 }
--- backend/src/main/java/com/mirror/backend/common/utils/ApiUtils.java
+++ backend/src/main/java/com/mirror/backend/common/utils/ApiUtils.java
@@ -1,9 +1,11 @@
 package com.mirror.backend.common.utils;
 
-import lombok.Getter;
-import lombok.Setter;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.springframework.http.HttpStatus;
 
 public class ApiUtils {
+
     public static <T> ApiResult<T> success(T response) {
         return new ApiResult<>(true, response, null);
     }
@@ -12,24 +14,44 @@ public class ApiUtils {
         return new ApiResult<>(false, response, null);
     }
 
-    public static ApiResult<?> error(String message, int status) {
+    public static ApiResult<?> error(Throwable throwable, HttpStatus status) {
+        return new ApiResult<>(false, null, new ApiError(throwable, status));
+    }
+
+    public static ApiResult<?> error(String message, HttpStatus status) {
         return new ApiResult<>(false, null, new ApiError(message, status));
     }
 
-    @Getter
-    @Setter
     public static class ApiError {
         private final String message;
         private final int status;
 
-        public ApiError(String message, int status) {
+        ApiError(Throwable throwable, HttpStatus status) {
+            this(throwable.getMessage(), status);
+        }
+
+        ApiError(String message, HttpStatus status) {
             this.message = message;
-            this.status = status;
+            this.status = status.value();
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public int getStatus() {
+            return status;
+        }
+
+        @Override
+        public String toString() {
+            return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                    .append("message", message)
+                    .append("status", status)
+                    .toString();
         }
     }
 
-    @Getter
-    @Setter
     public static class ApiResult<T> {
         private final boolean success;
         private final T response;
@@ -39,6 +61,27 @@ public class ApiUtils {
             this.success = success;
             this.response = response;
             this.error = error;
+        }
+
+        public boolean isSuccess() {
+            return success;
+        }
+
+        public ApiError getError() {
+            return error;
+        }
+
+        public T getResponse() {
+            return response;
+        }
+
+        @Override
+        public String toString() {
+            return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                    .append("success", success)
+                    .append("response", response)
+                    .append("error", error)
+                    .toString();
         }
     }
 }
