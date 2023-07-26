179870eb42b18750da1aa702f997ecc476f4512b
황주원 <woneee99@naver.com>
2023-07-26 14:14:22 +0900

    feat: my Task 전체 조회

--- backend/src/main/java/com/mirror/backend/api/service/CalendarService.java
+++ backend/src/main/java/com/mirror/backend/api/service/CalendarService.java
@@ -19,23 +19,6 @@ public class CalendarService {
     private final String HTTP_REQUEST_PRE = "https://www.googleapis.com/calendar/v3/calendars/";
     private final String HTTP_REQUEST_POST = "/events";
 
-    public String getCalendarList(String accessToken){
-        try {
-            String jsonData = "";
-
-            URL url = new URL(HTTP_REQUEST + "?access_token=" + accessToken);
-
-            BufferedReader bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
-
-            String line;
-            while((line = bf.readLine()) != null){
-                jsonData+=line;
-            }
-            return jsonData;
-        } catch(Exception e) {
-            return "error";
-        }
-    }
 
     public Event getMyCalendar(String accessToken, String calendarId) {
         try {
