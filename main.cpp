315e4b9da13cc9bb166f322876cb5cb43f7fefbf
신성환 <hw2ny1@gmail.com>
2023-07-27 01:08:31 +0900

    fix : fix header

--- main.cpp
+++ main.cpp
@@ -21,8 +21,8 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/videoio.hpp>
-#include "opencv2/highgui.hpp"
-#include "opencv2/core.hpp"
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
 // Serial
 #include <Windows.h>
 #include <atlstr.h>
