ff9155046b2d4e7359552cb3213f423e30d3a756
이진형 <wlsgud4084@naver.com>
2023-07-26 10:02:55 +0900

    feat: Add realtime clock
    새로고침없이 실시간으로 현재시각이 반영된 시계 추가

--- MainPage/src/components/CurrentDate.jsx
+++ MainPage/src/components/CurrentDate.jsx
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function CurrentDate(props) {
-  const now = new Date();
-  
-  const year = now.getFullYear();
-  const month = now.getMonth() + 1;
-  const date = now.getDate();
-  const week = ['일', '월', '화', '수', '목', '금', '토'];
-  const dayOfWeek = week[now.getDay()]
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [blink, setBlink] = useState(true);
 
-  const hour = now.getHours();
-  const hour24 = hour > 12 ? hour - 12  : hour
-  const minute = now.getMinutes();
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000); // 1초마다 상태 업데이트
+
+    const blinkInterval = setInterval(() => {
+      setBlink(prevBlink => !prevBlink);
+    }, 500); // 0.5초마다 상태 토글
+
+    // 컴포넌트가 언마운트되면 interval 정리 (클리어)
+    return () => {
+      clearInterval(interval);
+      clearInterval(blinkInterval);
+    };
+  }, []);
+
+  const year = currentTime.getFullYear();
+  const month = currentTime.getMonth() + 1;
+  const date = currentTime.getDate();
+  const week = ['일', '월', '화', '수', '목', '금', '토'];
+  const dayOfWeek = week[currentTime.getDay()]
+
+  const hour = currentTime.getHours();
+  const hour24 = hour > 12 ? hour - 12 : hour
+  const minute = currentTime.getMinutes();
 
   const ampm = hour >= 12 ? 'PM' : 'AM';
 
   return (
     <div>
       <p>
-        { year }년 { month }월 { date }일 { dayOfWeek }요일
+        {year}년 {month}월 {date}일 {dayOfWeek}요일
       </p>
       <p>
-        { ampm } { hour24 } : { minute }
+        {blink ? `${ampm} ${hour24}:${minute}` : `${ampm} ${hour24} ${minute}`}
       </p>
     </div>
   );
