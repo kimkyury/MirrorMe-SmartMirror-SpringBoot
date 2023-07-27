1ccf019356f862de90653cb0340a7ca19661854b
이진형 <wlsgud4084@naver.com>
2023-07-27 17:54:17 +0900

    feat: Add axios
    axios 관련 코드 추가

--- Frontend/Mirror/react/src/components/ToDoList.jsx
+++ Frontend/Mirror/react/src/components/ToDoList.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function ToDoList(props) {
-  const [toDoList, setTodoList] = useState([
-    '일정 1',
-    '일정 2',
-    '일정 3',
-  ]);
+  const [schedule, setSchedule] = useState("");
+
+  useEffect(() => {
+    const accessToken = 'ya29.a0AbVbY6Pb7osab39m486LDiX1GSXzMuJTFH3RnXeQfe_pVjZDa-kog1X7olqnE6bkm7L8oAFS-KNwSO62RtyTz2kxPn9zyvFTSt70XM0ZNfjPsDEPhYtMjYSk1H_4yvs77dpVmnVMIJNIOqKecP4hl0O_z_RdaCgYKAYwSARMSFQFWKvPlkq4dLzwToy7XMYHfccyilA0163';
+
+    axios.get("http://192.168.30.158:8080/schedule/today", {
+      params: { accessToken : accessToken },
+    }).then((res) => {
+      console.log(res.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  },[]);
 
   return (
     <>
       <h3>오늘의 일정</h3>
-      { toDoList.map((item, index) => {
-        return <li>{ item }</li>
-      })}
     </>
   );
 }
