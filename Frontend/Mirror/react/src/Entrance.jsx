baa937192f15d36334618607afc418add737a921
이진형 <wlsgud4084@naver.com>
2023-07-27 11:42:48 +0900

    chore: Change repository name
    merge를 위한 폴더명 변경입니다

--- Frontend/Mirror/react/src/Entrance.jsx
+++ Frontend/Mirror/react/src/Entrance.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Entrance.css';
 
 function Entrance(props) {
   return (
--- Frontend/Mirror/react/src/LivingRoom.jsx
+++ Frontend/Mirror/react/src/LivingRoom.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './LivingRoom.css';
 
 function LivingRoom(props) {
   return (
