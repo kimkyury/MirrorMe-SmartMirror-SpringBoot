13bb6ecd2c68508fb5b799b4c653aa6c5511bdb7
황주원 <woneee99@naver.com>
2023-07-27 17:27:41 +0900

    feat: video get structure

--- backend/src/main/java/com/mirror/backend/api/dto/Message.java
+++ backend/src/main/java/com/mirror/backend/api/dto/Message.java
@@ -1,9 +1,19 @@
 package com.mirror.backend.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 public class Message {
     private String text;
+
+    @Getter
+    @AllArgsConstructor
+    public static class ResponseMessage {
+        private byte[] videoFile;
+        private byte[] voiceFile;
+        private int userId;
+        private int sendUserId;
+    }
 }
--- backend/src/main/java/com/mirror/backend/api/service/impl/VideoServiceImpl.java
+++ backend/src/main/java/com/mirror/backend/api/service/impl/VideoServiceImpl.java
@@ -1,5 +1,8 @@
 package com.mirror.backend.api.service.impl;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.tomcat.util.codec.binary.Base64;
+import com.mirror.backend.api.dto.Message;
 import com.mirror.backend.api.entity.VideoMessage;
 import com.mirror.backend.api.repository.VideoMessageRepository;
 import com.mirror.backend.api.service.VideoService;
@@ -7,11 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class VideoServiceImpl implements VideoService {
@@ -32,6 +35,34 @@ public class VideoServiceImpl implements VideoService {
     public int saveVideo(String videoPath, String voicePath) {
         VideoMessage message = new VideoMessage(1, 2, videoPath, voicePath);
         return videoMessageRepository.save(message).getUserId();
+    }
+
+    @Override
+    public List<Message.ResponseMessage> getVideo(int userId) {
+        Optional<VideoMessage> videoList = videoMessageRepository.findById(userId);
+        if(!videoList.isPresent()) return null;
+        List<VideoMessage> videoMessageList = videoList.stream().collect(Collectors.toList());
+
+        List<Message.ResponseMessage> responseMessages = new ArrayList<>();
+        for(VideoMessage videoMessage : videoMessageList) {
+            String videoPath = videoMessage.getVideoPath();
+            String voicePath = videoMessage.getVoicePath();
+
+            try {
+                InputStream inputStream = new FileInputStream(videoPath);
+                byte[] images = IOUtils.toByteArray(inputStream);
+                byte[] byteEnc64 = Base64.encodeBase64(images);
+                String imgStr = new String(byteEnc64, "UTF-8");
+
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+        return responseMessages;
     }
 
     @Override
