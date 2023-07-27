19df6a9c4d0a9a3ee1a9a417221f2772a60a7fda
황주원 <woneee99@naver.com>
2023-07-27 12:10:42 +0900

    feat: redis 연결 & 영상, 음성 메시지 저장

--- backend/src/main/java/com/mirror/backend/api/controller/VideoController.java
+++ backend/src/main/java/com/mirror/backend/api/controller/VideoController.java
@@ -4,10 +4,15 @@ import com.mirror.backend.api.dto.Message;
 import com.mirror.backend.api.service.VideoService;
 import com.mirror.backend.common.utils.ApiUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 import static com.mirror.backend.common.utils.ApiUtils.success;
 
@@ -18,10 +23,21 @@ public class VideoController {
     @Autowired
     private VideoService videoService;
 
+    @Value("${spring.servlet.multipart.location}")
+    private String filePath;
+
     @PostMapping("/message")
-    public ApiUtils.ApiResult<Integer> getMessage(@RequestBody Message text) {
+    public ApiUtils.ApiResult<Integer> getMessageCheck(@RequestBody Message text) {
         return success(videoService.matchVideo(text.getText()));
-    } 
+    }
 
+    @PostMapping
+    public ApiUtils.ApiResult<Integer> postMessage(MultipartFile videoFile, MultipartFile voiceFile) {
+        int userId = 1;
+        String videoPath = videoService.transferFile(videoFile, filePath);
+        String voicePath = videoService.transferFile(voiceFile, filePath);
 
+        videoService.saveVideo(videoPath, voicePath);
+        return success(1);
+    }
 }
