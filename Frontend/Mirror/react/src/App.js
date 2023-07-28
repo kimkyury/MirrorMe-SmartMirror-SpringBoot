99f06bddd01844d0817f8cff3046c5e969393ed6
이소정 <stuj7913@naver.com>
2023-07-28 10:50:34 +0900

    chore : video modal css and design

--- Frontend/Mirror/react/src/App.js
+++ Frontend/Mirror/react/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 // import Container from 'react-bootstrap/Container';
 // import Row from 'react-bootstrap/Row';
@@ -8,11 +9,26 @@ import './App.css';
 import Weather from './components/Weather';
 import CurrentDate from './components/CurrentDate';
 import ToDoList from './components/ToDoList';
-import VideoMessage from './components/VideoMessage';
+import VideoMessage from './VideoMessage';
 import FavoriteNews from './components/FavoriteNews';
 import Character from './components/Character';
 
 function App() {
+  const [isVideoMessageVisible, setVideoMessageVisible] = useState(false);
+  const [isLivingRoomVisible, setLivingRoomVisible] = useState(false);
+
+  const [user, setUser] = useState('이소정');
+
+  // Open Living Room Modal
+  const openVideoMessageModal = () => {
+    setVideoMessageVisible(true);
+  };
+
+  // Close Living Room Modal
+  const closeVideoMessageModal = () => {
+    setVideoMessageVisible(false);
+  };
+  
   return (
     <div className="App">
       <div className="container">
@@ -28,9 +44,11 @@ function App() {
       <div className="bordered">
         <ToDoList></ToDoList>
       </div>
-      <div className="bordered">
-        <VideoMessage></VideoMessage>
-      </div>
+      {/* <div className="bordered">
+        {isVideoMessageVisible && <VideoMessage isVisible={true} onClose={closeVideoModal} />}
+      </div> */}
+      <button onClick={openVideoMessageModal}>Open Video Message Modal</button>
+      {isVideoMessageVisible && <VideoMessage onClose={closeVideoMessageModal} />}
       <div className="bordered">
         <FavoriteNews></FavoriteNews>
       </div>
