73c36515234f87a2206f32956c127925db7994c6
황주원 <woneee99@naver.com>
2023-07-29 17:25:15 +0900

    feat: video post, get api

--- backend/src/main/java/com/mirror/backend/api/repository/VideoMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mirror.backend.api.repository;
-
-import com.mirror.backend.api.entity.VideoMessage;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VideoMessageRepository extends CrudRepository<VideoMessage, Integer> {
-}
