8f29fcaffdc4d80005b7803764deae94515bd088
김규리 <mandarining0918@gmail.com>
2023-07-30 19:26:34 +0900

    feat: Get Access/Refresh Token

--- backend/src/main/java/com/mirror/backend/api/info/GoogleOAuth.java
+++ backend/src/main/java/com/mirror/backend/api/info/GoogleOAuth.java
@@ -9,25 +9,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class GoogleOAuth {
 
-    private final String googleLoginUrl = "https://accounts.google.com";
-    private final String googleEndPoint = "https://accounts.google.com/o/oauth2/v2/auth";
-    private final String GOOGLE_TOKEN_REQUEST_URL = "";
-    private final String GOOGLE_USERINFO_REQUEST_URL = "";
+
+    public final String REQUEST_AUTH_CODE_URL = "https://accounts.google.com/o/oauth2/v2/auth";
+    // 갱신시: POST
+    public final String REQUEST_TOKEN_URL = "https://oauth2.googleapis.com/token";
+
+    public final String SCOPE_CALENDAR = "https://www.googleapis.com/auth/calendar.events.readonly";
+    public final String SCOPE_TASK = "https://www.googleapis.com/auth/tasks.readonly";
+
 
     @Value("${google.client-id}")
-    private String googleClientId;
-
-    @Value("${google.redirect-uri-oauth}")
-    private String googleRedirectUriOauth;
-
-    @Value("${google.redirect-uri-token}")
-    private String googleRedirectUriToken;
-
-    @Value("${google.redirect-uri-code}")
-    private String googleRedirectUriCode;
-
-
+    private String clientId;
     @Value("${google.client-secret}")
-    private String googleClientSecret;
+    private String clientSecret;
+
+
+    // receive AuthorizationCode
+    @Value("${google.redirect-uri}")
+    private String redirectUri;
+
+    // receive Authorization Access/Refresh Token
+//    @Value("${google.redirect-uri-token}")
+//    private String redirectUriToken;
+
 
 }
