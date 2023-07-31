34d32491c878a1ed14c3e6dc5bced87298b92efe
김규리 <mandarining0918@gmail.com>
2023-07-31 17:09:59 +0900

    feat: ReIssue AccessToken When AccessToken expires

--- backend/src/main/java/com/mirror/backend/api/controller/CalendarController.java
+++ backend/src/main/java/com/mirror/backend/api/controller/CalendarController.java
@@ -1,19 +1,13 @@
 package com.mirror.backend.api.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mirror.backend.api.dto.Event;
 import com.mirror.backend.api.service.CalendarService;
-import com.mirror.backend.api.service.RestJsonService;
 import com.mirror.backend.common.utils.ApiUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,18 +29,18 @@ public class CalendarController {
 
     @GetMapping
     @Operation(summary = "회원 캘린더 전체 조회", description = "회원 login시 받는 code를 이용하여 회원의 전체 캘린더를 조회합니다.")
-    public ApiUtils.ApiResult<Event> getSchedule(@RequestParam("code") String code) {
-        RestJsonService restJsonService = new RestJsonService();
-        String accessTokenData = null;
-        try {
-            accessTokenData = restJsonService.getAccessTokenJsonData(code, CLIENT_ID, CLIENT_SECRET, 0);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+    public ApiUtils.ApiResult<Event> getSchedule(@RequestHeader("access_token") String accessToken) {
+//        RestJsonService restJsonService = new RestJsonService();
+//        String accessTokenData = null;
+//        try {
+//            accessTokenData = restJsonService.getAccessTokenJsonData(code, CLIENT_ID, CLIENT_SECRET, 0);
+//        } catch (JsonProcessingException e) {
+//            throw new RuntimeException(e);
+//        }
 
-        JSONObject accessTokenjsonObject = new JSONObject(accessTokenData);
+//        JSONObject accessTokenjsonObject = new JSONObject(accessTokenData);
 
-        String accessToken = accessTokenjsonObject.get("access_token").toString();
+//        String accessToken = accessTokenjsonObject.get("access_token").toString();
         System.out.println("accessToken = " + accessToken);
         // access_token을 받는다면 이 전 코드는 삭제
         Event resData = calendarService.getMyCalendar(accessToken, "primary");
--- backend/src/main/java/com/mirror/backend/api/dto/ResponseReissueTokenDto.java
+++ backend/src/main/java/com/mirror/backend/api/dto/ResponseReissueTokenDto.java
@@ -8,21 +8,17 @@ import lombok.Setter;
 @Setter
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class ResponseLogin {
+public class ResponseReissueTokenDto {
 
-    @JsonProperty("isInitLoginUser")
-    private int isInitLoginUser;
     @JsonProperty("accessToken")
     private String accessToken;
     @JsonProperty("refreshToken")
     private String refreshToken;
 
-
     @Override
     public String toString() {
-        return "ResponseLogin{" +
-                "isInitLoginUser=" + isInitLoginUser +
-                ", accessToken='" + accessToken + '\'' +
+        return "ResponseReissueToken{" +
+                "accessToken='" + accessToken + '\'' +
                 ", refreshToken='" + refreshToken + '\'' +
                 '}';
     }
