65243f691ce022c76262146f4d5bfc086986d673
이소정 <stuj7913@naver.com>
2023-08-01 10:55:24 +0900

    refactor : application router view refactoring

---
+++ Frontend/Application/flutter_application_1/lib/component/Settings.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // 설정 컴포넌트의 내용을 작성
+    return Center(
+      child: Text('Page 5'),
+    );
+  }
+}
