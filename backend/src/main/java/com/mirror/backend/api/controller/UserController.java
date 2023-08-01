f0c4605922d4dc8eec21e1a4fbbf0e515553c647
김규리 <mandarining0918@gmail.com>
2023-08-01 11:53:27 +0900

    feat: Write Controller for User ProfileImg upload

--- backend/src/main/java/com/mirror/backend/api/controller/UserController.java
+++ backend/src/main/java/com/mirror/backend/api/controller/UserController.java
@@ -5,11 +5,14 @@ import com.mirror.backend.api.entity.User;
 import com.mirror.backend.api.service.OAuthService;
 import com.mirror.backend.api.service.UserService;
 import com.mirror.backend.common.utils.ApiUtils;
+import com.mirror.backend.common.utils.Constants.Result;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import static com.mirror.backend.common.utils.ApiUtils.fail;
 import static com.mirror.backend.common.utils.ApiUtils.success;
 
 
@@ -24,17 +27,33 @@ public class UserController {
     @Autowired
     private OAuthService oAuthService;
 
-
     @PostMapping("/signup")
-    public ApiUtils.ApiResult<Integer> signUp( @RequestHeader("access_token") String accessToken, @RequestBody RequestCreateUserDto requestCreateUserDto) {
+    public ApiUtils.ApiResult<String> signUp(@RequestHeader("access_token") String accessToken,
+                                             @RequestBody RequestCreateUserDto requestCreateUserDto) {
+        // 이메일 찾기
+        String userEmail = oAuthService.getUserEmailFromAccessToken(accessToken);
+        // 해당 이메일을 가진 유저의 정보 업데이트하기
+        int result = userService.updateInitUser(userEmail, requestCreateUserDto);
 
+        if ( result == Result.FAIL ){
+            return fail("user 추가 정보 생성 실패");
+        }
+        return success("User 추가 정보 생성 성공");
+    }
+
+    @PostMapping("/profile/img")
+    public ApiUtils.ApiResult<String> updateProfileImage(@RequestHeader("access_token") String accessToken,
+                                             @RequestPart(value = "file") MultipartFile file) {
 
         // 이메일 찾기
         String userEmail = oAuthService.getUserEmailFromAccessToken(accessToken);
         // 해당 이메일을 가진 유저의 정보 업데이트하기
-        userService.updateInitUser(userEmail, requestCreateUserDto);
+        int result = userService.uploadProfileImage(userEmail, file);
 
-        return null;
+        if ( result == Result.FAIL ){
+            return fail("user Profile Img 업데이트 실패");
+        }
+        return success("User Profile Img 업데이트 성공");
     }
 
 
--- backend/src/main/java/com/mirror/backend/api/service/UserService.java
+++ backend/src/main/java/com/mirror/backend/api/service/UserService.java
@@ -7,11 +7,18 @@ import com.mirror.backend.api.entity.keys.InterestKey;
 import com.mirror.backend.api.repository.InterestsRepository;
 import com.mirror.backend.api.repository.RedisUserTokenRepository;
 import com.mirror.backend.api.repository.UserRepository;
+import com.mirror.backend.common.utils.Constants.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,31 +43,39 @@ public class UserService {
                         new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
     }
 
+    public User createUser(String userEmail){
+        User user = new User();
+        user.setUserEmail(userEmail);
+        return userRepository.save(user);
+    }
+
     public int updateInitUser(String userEmail, RequestCreateUserDto requestCreateUserDto ){
 
         Optional<User> user = userRepository.findByUserEmail(userEmail);
+        // userId찾기
+        Long userId = user.get().getUserId();
 
-        if ( user.isEmpty()){
+        if ( user.isEmpty())
             return FAIL;
-        }
 
         String userName = requestCreateUserDto.getUserName();
         String userNickname = requestCreateUserDto.getUserNickname();
         List<Long> interestCodes = requestCreateUserDto.getInterestCodes();
+        Long householdId = requestCreateUserDto.getHouseholdId();
 
-        Long userId = user.get().getUserId();
+        // 저장된 파일의 경로를 DB에 저장
 
+        // uesrTable Update
         user.ifPresent(selectUser -> {
             selectUser.setUserName(userName);
             selectUser.setUserNickname(userNickname);
+            selectUser.setHouseholdId(householdId);
+            selectUser.setCreateAt(LocalDateTime.now());
             userRepository.save(selectUser);
         });
 
-        // userId찾기
-
-
-        // interests를 저장하도록 하기 (multyKey1; userId, multiKey2: interestId)
-        // 복합키 생성
+        // interests Create (multyKey1; userId, multiKey2: interestId)
+        // 복합키 이용
         for(int i =0; i<interestCodes.size(); i++){
             InterestKey interestKey = new InterestKey();
             interestKey.setUserId(userId);
@@ -76,14 +91,39 @@ public class UserService {
         return SUCCESS;
     }
 
-    public User createUser(String userEmail){
-        User user = new User();
-        user.setUserEmail(userEmail);
 
-        return userRepository.save(user);
+
+    public int uploadProfileImage(String userEmail, MultipartFile file) {
+        Optional<User> user = userRepository.findByUserEmail(userEmail);
+
+        String UPLOAD_DIR = "ImgTest/";  //TODO: 서버내의 절대경로로 바꿀 것
+
+        Path filePath = null;
+
+        try {
+
+            Path resourceDirectory = Paths.get("src","main","resources", UPLOAD_DIR);
+            String absolutePath = resourceDirectory.toFile().getAbsolutePath();
+
+            filePath = Paths.get(absolutePath, file.getOriginalFilename());
+            Files.write(filePath, file.getBytes());
+
+        } catch (IOException e) {
+            // 에러 처리
+            e.printStackTrace();
+        }
+
+        System.out.println("이미지파일 저장경로: " + filePath.toString());
+        Path finalFilePath = filePath;
+
+        user.ifPresent(selectUser -> {
+            selectUser.setProfileImageUrl(finalFilePath.toString());
+            selectUser.setModifiedAt(LocalDateTime.now());
+            userRepository.save(selectUser);
+        });
+
+        return Result.SUCCESS;
     }
-
-
 
     public boolean isExistUser(String email){
 
