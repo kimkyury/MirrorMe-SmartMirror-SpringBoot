3194358a3cd873ab386f434892a31199ed08856a
황주원 <woneee99@naver.com>
2023-08-02 17:23:15 +0900

    test: video constructor add

--- backend/src/main/java/com/mirror/backend/api/entity/VideoMessage.java
+++ backend/src/main/java/com/mirror/backend/api/entity/VideoMessage.java
@@ -1,27 +1,24 @@
 package com.mirror.backend.api.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 @Getter
-@ToString
+@NoArgsConstructor
 @RedisHash(value = "video", timeToLive = 30)
 public class VideoMessage {
 
     @Id
-    private String userId;
-    private String sendUserId;
+    private String userEmail;
+    private String sendUserEmail;
     private String fileName;
     private String type;
 
     @Builder
-    public VideoMessage(String userId, String sendUserId, String fileName, String type) {
-        this.userId = userId;
-        this.sendUserId = sendUserId;
+    public VideoMessage(String userEmail, String sendUserEmail, String fileName, String type) {
+        this.userEmail = userEmail;
+        this.sendUserEmail = sendUserEmail;
         this.fileName = fileName;
         this.type = type;
     }
