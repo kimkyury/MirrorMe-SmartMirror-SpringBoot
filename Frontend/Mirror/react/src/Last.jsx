c892eb6e27f0b450d36818112cfa57e227fc8016
이진형 <wlsgud4084@naver.com>
2023-08-03 11:17:42 +0900

    feat: Add NavBar in mirror
    거울 내 NavBar 추가

--- Frontend/Mirror/react/src/Last.jsx
+++ Frontend/Mirror/react/src/Last.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+
+import NavBar from './NavBar';
 import Emotions from './components/Emotions';
 
 
 function Last(props) {
   return (
     <div className="App">
+      <div>
+        <NavBar></NavBar>
+      </div>
       <div>
         <Emotions></Emotions>
       </div>
