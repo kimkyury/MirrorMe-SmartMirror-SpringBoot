15e6399df25047eae36f06ba6028d6ab42d272e9
이소정 <stuj7913@naver.com>
2023-08-04 09:49:06 +0900

    feat : time

--- Frontend/Mirror_ver2/final/src/App.js
+++ Frontend/Mirror_ver2/final/src/App.js
@@ -1,25 +1,34 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const options = { hour12: true, hour: "2-digit", minute: "2-digit", second: "2-digit" };
+  const formattedTime = currentTime.toLocaleTimeString("en-US", options);
+
+  const [timePart, AMPM] = formattedTime.split(" ");
+  const formattedTimeWithAmPm = `${AMPM} ${timePart}`;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="time">{formattedTimeWithAmPm}</div>
       </header>
     </div>
   );
 }
 
 export default App;
+
