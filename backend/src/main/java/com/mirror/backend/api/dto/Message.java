f643456c89715edd57c2dcf3fc8ccc3195184c88
황주원 <woneee99@naver.com>
2023-08-04 17:52:59 +0900

    feat: video get logic separation

--- backend/src/main/java/com/mirror/backend/api/dto/Message.java
+++ backend/src/main/java/com/mirror/backend/api/dto/Message.java
@@ -12,13 +12,15 @@ public class Message {
 
     @Getter
     public static class ResponseMessage {
+        private int idx;
         private String videoFile;
         private String userEmail;
         private String sendUserEmail;
         private String date;
 
         @Builder
-        public ResponseMessage(String videoFile, String userEmail, String sendUserEmail, String date) {
+        public ResponseMessage(int idx, String videoFile, String userEmail, String sendUserEmail, String date) {
+            this.idx = idx;
             this.videoFile = videoFile;
             this.userEmail = userEmail;
             this.sendUserEmail = sendUserEmail;
