00debaf011872de0e7ba318f65da78ee1c7b504c
이진형 <wlsgud4084@naver.com>
2023-08-04 09:44:11 +0900

    feat: Add component animation
    특정 상황 시 컴포넌트 애니메이션 연출 구현

--- Frontend/Mirror_ver2/final/src/App.js
+++ Frontend/Mirror_ver2/final/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import './App.css';
 
+import TodayWeather from './components/TodayWeather';
+import WeekWeather from './components/WeekWeather';
+
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsVisible(prevState => !prevState);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <button onClick={toggleVisibility}>토글 컴포넌트</button>
+      <div className="animated-container">
+        <CSSTransition
+          in={isVisible}
+          timeout={300}
+          classNames="slide"
+          unmountOnExit
         >
-          Learn React
-        </a>
-      </header>
+          <div className="animated-content">
+            <div className="bordered">
+              <TodayWeather />
+            </div>
+            <div className="bordered">
+              <WeekWeather />
+            </div>
+          </div>
+        </CSSTransition>
+      </div>
     </div>
   );
 }
