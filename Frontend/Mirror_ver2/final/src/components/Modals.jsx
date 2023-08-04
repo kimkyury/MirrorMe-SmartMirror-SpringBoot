537693be56a586dcbea031a4aa28d11a78fc0397
이소정 <stuj7913@naver.com>
2023-08-04 15:20:43 +0900

    feat : youtube modal

--- Frontend/Mirror_ver2/final/src/components/Modals.jsx
+++ Frontend/Mirror_ver2/final/src/components/Modals.jsx
@@ -8,35 +8,29 @@ function ModalBottons(props) {
   const [isMessageModalOpen, setIsMessageModalOpen] = useState(false);
   const [isYoutubeModalOpen, setIsYoutubeModalOpen] = useState(false);
 
-  const openQRModal = () => {
-    setIsQRModalOpen(true);
+  const toggleQRModal = () => {
+    setIsQRModalOpen(prevState => !prevState);
     setIsMessageModalOpen(false);
     setIsYoutubeModalOpen(false);
   };
 
-  const openMessageModal = () => {
+  const toggleMessageModal = () => {
     setIsQRModalOpen(false);
-    setIsMessageModalOpen(true);
+    setIsMessageModalOpen(prevState => !prevState);
     setIsYoutubeModalOpen(false);
   };
 
-  const openYoutubeModal = () => {
+  const toggleYoutubeModal = () => {
     setIsQRModalOpen(false);
     setIsMessageModalOpen(false);
-    setIsYoutubeModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsQRModalOpen(false);
-    setIsMessageModalOpen(false);
-    setIsYoutubeModalOpen(false);
+    setIsYoutubeModalOpen(prevState => !prevState);
   };
 
   return (
     <div>
-      <Button className="btn" onClick={openQRModal}>QR</Button>
-      <Button className="btn" onClick={openMessageModal}>메세지</Button>
-      <Button className="btn" onClick={openYoutubeModal}>유튜브</Button>
+      <Button className="btn" onClick={toggleQRModal}>QR</Button>
+      <Button className="btn" onClick={toggleMessageModal}>메세지</Button>
+      <Button className="btn" onClick={toggleYoutubeModal}>유튜브</Button>
 
       {/* QR Modal */}
       {isQRModalOpen && (
@@ -44,7 +38,6 @@ function ModalBottons(props) {
           {/* Add QR modal content here */}
           <h2>QR 모달</h2>
           <p>QR 코드를 여기에 표시합니다.</p>
-          <button onClick={closeModal}>닫기</button>
         </div>
       )}
 
@@ -54,7 +47,6 @@ function ModalBottons(props) {
           {/* Add Message modal content here */}
           <h2>메세지 모달</h2>
           <p>메세지를 여기에 표시합니다.</p>
-          <button onClick={closeModal}>닫기</button>
         </div>
       )}
 
@@ -63,8 +55,15 @@ function ModalBottons(props) {
         <div className="modal">
           {/* Add Youtube modal content here */}
           <h2>유튜브 모달</h2>
-          <p>유튜브 동영상을 여기에 표시합니다.</p>
-          <button onClick={closeModal}>닫기</button>
+          <iframe
+            width="800"
+            height="400"
+            src="https://www.youtube.com/embed/LqME1y6Mlyg"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe><br />
         </div>
       )}
     </div>
