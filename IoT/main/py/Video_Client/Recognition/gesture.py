7198595e43089e26e4c0ae5faac0017e9bb6cf69
hw2ny1 <hw2ny1@gmail.com>
2023-08-06 21:57:42 +0900

    feat : hands motion tracking
    손 움직임으로 좌우 넘기기 기능 완성

--- IoT/main/py/Video_Client/Recognition/gesture.py
+++ IoT/main/py/Video_Client/Recognition/gesture.py
@@ -14,23 +14,39 @@ def getgesture():
 
     compareIndex = [[6,8],[10,12],[14,16],[18,20]]
     open = [True, False, False, False, (0,0)]
-    gesture = [[True, True, True, True, (0,0), "Recording Audio"],
-            [True, True, False, False, (0,0), "Recording Video"],
-            [True, False, False, True, (0,0), "Yo"],
+    gesture = [[True, True, True, True, (0,0), "5"],
+            [True, True, False, False, (0,0), "V"],
+            [True, False, False, True, (0,0), "A"],
             [False, False, False, False, (0,0), "EXIT"]]
     
-    result = deque([None for _ in range(11)])
-    distance = deque([(0, 0) for _ in range(11)])
+    result = deque([None for _ in range(26)])
+    distance = deque(['remain' for _ in range(8)])
+
+    last_x = 0.5
+
     while True:
         result.popleft()
-        distance.popleft()
 
         success, img = cap.read()
         imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
         results = my_hands.process(imgRGB)
         if results.multi_hand_landmarks:
             handLms = results.multi_hand_landmarks[0]
-            distance.append((handLms.landmark[0].x, handLms.landmark[0].y))
+            
+            if last_x - handLms.landmark[12].x > 0.03:
+                distance.append("right")
+                distance.popleft()
+                last_x = handLms.landmark[12].x
+            elif last_x - handLms.landmark[12].x < -0.03:
+                distance.append("left")
+                distance.popleft()
+                last_x = handLms.landmark[12].x
+            else:
+                distance.append("remain")
+                distance.popleft()
+                last_x = handLms.landmark[12].x
+            
+            print(last_x)
 
             for i in range(0,4):
                 open[i] = (dist(handLms.landmark[0].x,handLms.landmark[0].y,
@@ -49,12 +65,23 @@ def getgesture():
                     result.append(gesture[i][5])
                     break
         
+
         if len(result) < 10:
             result.append(None)
-            distance.append((0,0))
 
         cv2.imshow("hand",img)
         cv2.waitKey(1)
 
-        print(result)
-        print(distance)
+        ret = max(set(result), key=result.count)
+
+        if ret == "5":
+            ret = max(set(distance), key=distance.count)
+            if ret == "remain":
+                continue
+            return ret
+        elif ret == None:
+            pass
+        else:
+            return ret
+
+
