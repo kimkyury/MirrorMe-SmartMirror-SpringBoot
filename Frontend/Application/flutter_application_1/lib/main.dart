c4de47488e5c088b220af57bdd21f69a6001d125
이진형 <wlsgud4084@naver.com>
2023-08-08 17:53:49 +0900

    fix: google api
    유저 api 관련 수정

--- Frontend/Application/flutter_application_1/lib/main.dart
+++ Frontend/Application/flutter_application_1/lib/main.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './splash_screen.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+
+  // const baseUrl = "http://i9e101.p.ssafy.io:8080";
+  // final queryParams = {
+  //   'numOfRows': '100',
+  //   'pageNo': '1',
+  // };
+
+  // final uri = Uri.parse('$baseUrl/weather/mid').replace(queryParameters: queryParams);
+
+  // try {
+  //   var response = await http.get(uri);
+  //   var statusCode = response.statusCode;
+  //   var responseHeaders = response.headers;
+  //   var responseBody = response.body;
+
+  //   print("statusCode: $statusCode");
+  //   print("responseHeaders: $responseHeaders");
+  //   print("responseBody: $responseBody");
+  // } catch (e) {
+  //   print("Error: $e");
+  // }
 }
 
 class MyApp extends StatelessWidget {
