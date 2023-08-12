2e8f6609e2fb7cf931c9a01673554cf6a6d4ead9
김규리 <mandarining0918@gmail.com>
2023-08-12 15:16:14 +0900

    feat: Add getTodayMMDD Method

--- backend/src/main/java/com/mirror/backend/common/utils/EtcUtil.java
+++ backend/src/main/java/com/mirror/backend/common/utils/EtcUtil.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 
 public class EtcUtil {
@@ -13,5 +14,12 @@ public class EtcUtil {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");  // 원하는 날짜 형식 정의
         String formattedDate = today.format(formatter);  // 날짜를 원하는 형식으로 변환
         return formattedDate;
+    }
+
+    public static String getTodayMMDD(){
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMdd");
+        String formattedDte = today.format(formatter);
+        return formattedDte;
     }
 }
