46b5726cb59717e96347d5349d88b33a4cf9a9bb
김규리 <mandarining0918@gmail.com>
2023-08-14 21:44:54 +0900

    Refac: Remove UnUseFiles
    Former-commit-id: 49667aa94a8ee971378f5fe3bc26008d7a26eb47 [formerly 4568bd3a16d39f7f24ce476a728886175d42b222]
    Former-commit-id: 15b7d28c9a06845398fae0c9a07ffba5dae07ded

--- backend/src/main/java/com/mirror/backend/common/utils/ChatGptUtil.java
+++ backend/src/main/java/com/mirror/backend/common/utils/ChatGptUtil.java
@@ -5,6 +5,7 @@ import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.completion.chat.ChatMessageRole;
 import com.theokanning.openai.service.OpenAiService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -12,15 +13,11 @@ import java.util.HashMap;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class ChatGptUtil {
 
     public final ChatGPT chatGPT;
     private final OpenAiService openAiService;
-
-    public ChatGptUtil(ChatGPT chatGPT, OpenAiService openAiService) {
-        this.chatGPT = chatGPT;
-        this.openAiService = openAiService;
-    }
 
     public String createMessage(String question){
         List<ChatMessage> messages = new ArrayList<>();
@@ -28,7 +25,6 @@ public class ChatGptUtil {
         // TODO: MessageRole의 USER와 SYSTEM의 차이를 모르겠음
         ChatMessage chatMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(),
                 question);
-
         messages.add(chatMessage);
 
         ChatCompletionRequest chatCompletionRequest = createChatCompletionRequest(messages);
@@ -44,7 +40,6 @@ public class ChatGptUtil {
 
     private ChatCompletionRequest createChatCompletionRequest(List<ChatMessage> messages){
 
-        // ChatGPT config
         ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder()
                 .model("gpt-3.5-turbo-0613")
                 .messages(messages)
@@ -54,5 +49,4 @@ public class ChatGptUtil {
                 .build();
         return chatCompletionRequest;
     }
-
 }
