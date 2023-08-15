ecbe3db63a863f4801513eed3482816a6d244b9d
이진형 <wlsgud4084@naver.com>
2023-08-15 17:38:04 +0900

    fix: video message
    Former-commit-id: 56452328495137612cdef6eafb12c64428ca0875 [formerly 41a7cb829d42f6e46b52c182d54bbce15c1f38ee]
    Former-commit-id: 888f4076386a73441c734ab5e9e0fa08af033e93

--- Frontend/Mirror_ver2/final/src/components/VideoMessagePlus.jsx
+++ Frontend/Mirror_ver2/final/src/components/VideoMessagePlus.jsx
@@ -6,30 +6,11 @@ function VideoMessagePlus(props) {
   const userAccessToken = props.userAccessToken;
   const userRefreshToken = props.userRefreshToken;
 
-  const [videoUrl, setVideoUrl] = useState('');
-
-  useEffect(() => {
-    const fetchMessageMetadata = async () => {
-      try {
-        const res = await axios.get(`/video/message`, {
-          headers: { access_token: userAccessToken },
-          params: { videoId: videoId },
-          withCredentials: true,
-        });
-
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    
-    fetchMessageMetadata();
-  }, [userAccessToken]);
-
   return (
     <div className="video-message-container">
       <div className="message-content">
         <video id="video-player" controls autoPlay style={{ width: '800px', height: '400px' }}>
-          <source src={`video/message?videoId=${videoId}`} type="video/mp4" />
+          <source src={`api/iot/message?videoId=${videoId}`} type="video/mp4" />
         </video>
       </div>
     </div>
