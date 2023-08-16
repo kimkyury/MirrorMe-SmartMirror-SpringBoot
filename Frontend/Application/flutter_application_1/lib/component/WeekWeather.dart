027b8ce0f49b63448e5cdedf060d79e1a6ccfed0
이소정 <stuj7913@naver.com>
2023-08-16 10:08:37 +0900

    chore : application weather frame
    Former-commit-id: 49c9ff5b9f2258ff9afad73b795b11d2fdfed7f0 [formerly b33485d094c613a758f1e947bff9734ffc922944]
    Former-commit-id: 6b15a6e7b8892e8afa7c5d005dc67c5f33865e5f

--- Frontend/Application/flutter_application_1/lib/component/WeekWeather.dart
+++ Frontend/Application/flutter_application_1/lib/component/WeekWeather.dart
@@ -4,6 +4,14 @@ class WeekWeather extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.amber,
+      width: 350,
+      height: 150,
+      margin: EdgeInsets.only(
+        left: 20,
+        right: 20,
+        top: 10,
+        ),
       child: Container(
           child: Text('주간 날씨'),
         )
