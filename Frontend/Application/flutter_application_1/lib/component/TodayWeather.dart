49c9ff5b9f2258ff9afad73b795b11d2fdfed7f0
이소정 <stuj7913@naver.com>
2023-08-16 10:08:37 +0900

    chore : application weather frame
    Former-commit-id: b33485d094c613a758f1e947bff9734ffc922944

--- Frontend/Application/flutter_application_1/lib/component/TodayWeather.dart
+++ Frontend/Application/flutter_application_1/lib/component/TodayWeather.dart
@@ -4,9 +4,18 @@ class TodayWeather extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.amber,
+      width: 350,
+      height: 150,
+      margin: EdgeInsets.only(
+        left: 20,
+        right: 20,
+        bottom: 10,
+        ),
       child: Container(
-          child: Text('오늘 날씨'),
-        )
-    );
+          // 오늘 날씨 정보 넣자
+          child: Text('일간 날씨'),
+        ),
+      );
   }
 }
