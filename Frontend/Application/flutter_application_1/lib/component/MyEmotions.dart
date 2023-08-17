802ed00c97bd74e4255bcaeae259976be5a60d7f
이소정 <stuj7913@naver.com>
2023-08-17 21:31:42 +0900

    feat : temp
    Former-commit-id: 47d653e1040b49b2baa9ecd9cc2a83ebfec7bcf9 [formerly 6850fbcdc993700243d9c9e1783fa5f08345d9c4]
    Former-commit-id: 4c4b73dce349d0871ca088417f8cef536042ed15

---
+++ Frontend/Application/flutter_application_1/lib/component/MyEmotions.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text('Emotion Frequency')),
+      body: MyEmotions(),
+    ),
+  ));
+}
+
+class MyEmotions extends StatelessWidget {
+  const MyEmotions({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<_ChartData> chartData = [
+      _ChartData('MON', 12, 10, 14, 20),
+      _ChartData('TUE', 14, 11, 18, 23),
+      _ChartData('WED', 16, 10, 15, 20),
+      _ChartData('THU', 12, 22, 16, 24),
+      _ChartData('FRI', 18, 16, 18, 20),
+      _ChartData('SAT', 16, 12, 20, 24),
+      _ChartData('SUN', 17, 16, 11, 21),
+    ];
+
+    return Container(
+      width: 350,
+      height: 280,
+      margin: EdgeInsets.only(
+        left: 20,
+        right: 20,
+      ),
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.fromLTRB(0, 20, 0, 10),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text( '지난 주 나의 감정',
+                  style: TextStyle(
+                    color: Color(0xff111111),
+                    fontSize: 20,
+                    fontFamily: 'NanumSquareRoundEB',
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.normal,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: SfCartesianChart(
+              primaryXAxis: CategoryAxis(),
+              series: <ChartSeries>[
+                StackedColumnSeries<_ChartData, String>(
+                  dataSource: chartData,
+                  xValueMapper: (_ChartData data, _) => data.day,
+                  yValueMapper: (_ChartData data, _) => data.emotion1,
+                ),
+                StackedColumnSeries<_ChartData, String>(
+                  dataSource: chartData,
+                  xValueMapper: (_ChartData data, _) => data.day,
+                  yValueMapper: (_ChartData data, _) => data.emotion2,
+                ),
+                StackedColumnSeries<_ChartData, String>(
+                  dataSource: chartData,
+                  xValueMapper: (_ChartData data, _) => data.day,
+                  yValueMapper: (_ChartData data, _) => data.emotion3,
+                ),
+                StackedColumnSeries<_ChartData, String>(
+                  dataSource: chartData,
+                  xValueMapper: (_ChartData data, _) => data.day,
+                  yValueMapper: (_ChartData data, _) => data.emotion4,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ChartData {
+  _ChartData(this.day, this.emotion1, this.emotion2, this.emotion3, this.emotion4);
+  final String day;
+  final num emotion1;
+  final num emotion2;
+  final num emotion3;
+  final num emotion4;  
+}
