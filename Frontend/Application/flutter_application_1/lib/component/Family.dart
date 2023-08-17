d77d4f1e7da9cd4c4eefd688a3d386b231273bef
이진형 <wlsgud4084@naver.com>
2023-08-17 12:11:59 +0900

    feat: application
    어플리케이션 기능 추가
    
    
    Former-commit-id: 1f208ead5c3b8de4e2931a1adc441f51cfacc22d [formerly cad2a02fb0b81689007faacf29c1220724f428a3]
    Former-commit-id: f97b8a07463a5f7b19aa71df3300b96fd06717d3

--- Frontend/Application/flutter_application_1/lib/component/Family.dart
+++ Frontend/Application/flutter_application_1/lib/component/Family.dart
@@ -4,11 +4,14 @@ class Family extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // 가족 컴포넌트의 내용을 작성
-    return ListView(
+    return Container(
+      color: Colors.white,
+      child: ListView(
       children: [
         SizedBox(height: 20,),
         // 우리 가족 목록
         Container(
+          padding: EdgeInsets.all(10),
           width: 350,
           height: 161,
           margin: EdgeInsets.only(
@@ -18,12 +21,12 @@ class Family extends StatelessWidget {
           child: Column(
             children: [
               Container(
-                padding: EdgeInsets.all(10),
                 margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text('Our Family',
+                    Text(
+                      '우리 가족 목록',
                       style: TextStyle(
                         color: Color(0xff111111),
                         fontSize: 20,
@@ -35,21 +38,61 @@ class Family extends StatelessWidget {
                   ],
                 ),
               ),
-            ],
-          ),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(14),
-            boxShadow: [
-              BoxShadow(
-                color: Color(0x29000000),
-                offset: Offset(0, 1),
-                blurRadius: 6,
-                spreadRadius: 0,
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(left: 10), // 들여쓰기
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        '가족 1',
+                        style: TextStyle(
+                          color: Color(0xff111111),
+                          fontSize: 16,
+                          fontFamily: 'NanumSquareRoundEB',
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        '가족 2',
+                        style: TextStyle(
+                          color: Color(0xff111111),
+                          fontSize: 16,
+                          fontFamily: 'NanumSquareRoundEB',
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        '가족 3',
+                        style: TextStyle(
+                          color: Color(0xff111111),
+                          fontSize: 16,
+                          fontFamily: 'NanumSquareRoundEB',
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
+              // 추가적인 질문과 답변을 이어서 나열하세요.
             ],
           ),
         ),
+        Divider(height: 10,),
         SizedBox(height: 20,),
         // 우리 가족 마음 온도
         Container(
@@ -68,7 +111,7 @@ class Family extends StatelessWidget {
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text('Our Family Temperture',
+                    Text('우리 가족 마음 온도',
                       style: TextStyle(
                         color: Color(0xff111111),
                         fontSize: 20,
@@ -82,20 +125,9 @@ class Family extends StatelessWidget {
               ),
             ],
           ),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(14),
-            boxShadow: [
-              BoxShadow(
-                color: Color(0x29000000),
-                offset: Offset(0, 1),
-                blurRadius: 6,
-                spreadRadius: 0,
-              ),
-            ],
-          ),
         ),
       ],
+    )
     );
   }
 }
