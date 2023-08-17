8aa75afe382de3c96c2564f0ebe0ac0c6dc5092b
이소정 <stuj7913@naver.com>
2023-08-17 15:15:30 +0900

    chore : temp
    Former-commit-id: e978aee74d61dc57aab7e9f8c8b5b4026bb7871c [formerly cd85362591734e32ee7c8f5b0cf2c15627b53d5c]
    Former-commit-id: c3b061fa8ed4e85daf1f961c3f8cfb8d2635cb7d

--- Frontend/Application/flutter_application_1/lib/component/TodayWeather.dart
+++ Frontend/Application/flutter_application_1/lib/component/TodayWeather.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:intl/intl.dart';
 
 class TodayWeather extends StatefulWidget {
   @override
