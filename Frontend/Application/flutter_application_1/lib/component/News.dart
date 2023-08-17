d8355155d04cfd368f5d2dca1d5d504d94356bca
이진형 <wlsgud4084@naver.com>
2023-08-18 04:23:01 +0900

    feat: Add coming soon

--- Frontend/Application/flutter_application_1/lib/component/News.dart
+++ Frontend/Application/flutter_application_1/lib/component/News.dart
@@ -1,171 +1,56 @@
 import 'package:flutter/material.dart';
-import './NewsTab.dart';
 
-class News extends StatelessWidget {
+class News extends StatefulWidget {
+  @override
+  _NewsState createState() => _NewsState();
+}
+
+class _NewsState extends State<News> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    )..repeat(reverse: true); // Repeats the animation in reverse (fade out, fade in)
+
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        SizedBox(height: 20,),
-        // 우리 가족 목록
-        Container(
-          padding: EdgeInsets.all(10), // 수정된 부분
-          width: 350,
-          height: 161,
-          margin: EdgeInsets.symmetric(horizontal: 20), // 수정된 부분
-          child: Column(
-            children: [
-              Container( // 키워드 별 컨테이너 한 개
-                padding: EdgeInsets.all(10),
-                height: 130,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Row( // 키워드 제목
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          child: Row(
-                            children: [
-                              Icon(Icons.sports_football, color: Colors.black,),
-                              SizedBox(width: 10,),
-                              Text('스포츠', style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 23,
-                                fontWeight: FontWeight.w500
-                              ),),
-                            ],
-                          ),
-                        ),
-                        Text('업데이트 시간') // 추후 업데이트 시간으로 변경
-                      ],
-                    ),
-                    Container( // 기사 목록
-                      child: Column(
-                      children: [
-                        Row( // 뉴스 기사 1
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('기사 제목 줄여서 출력하기 위한 예시...'),
-                            Text('언론사')
-                          ],
-                        ),
-                        Row( // 뉴스 기사 2
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('기사 제목 줄여서 출력하기 위한 예시...'),
-                            Text('언론사')
-                          ],
-                        ),
-                        Row( // 뉴스 기사 3
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('기사 제목 줄여서 출력하기 위한 예시...'),
-                            Text('언론사')
-                          ],
-                        ),
-                      ],
-                      ),
-                    ),
-                  ],
-                ),
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FadeTransition(
+              opacity: _animation,
+              child: Image.asset(
+                'lib/assets/MirrorMe_logo.png',// Adjust width as needed
               ),
-            ],
-          ),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(14),
-            boxShadow: [
-              BoxShadow(
-                color: Color(0x29000000),
-                offset: Offset(0, 1),
-                blurRadius: 6,
-                spreadRadius: 0,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Coming Soon',
+              style: TextStyle(
+                fontSize: 24,
+                fontFamily: 'NanumSquareRoundEB',
+                fontWeight: FontWeight.bold,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-        SizedBox(height: 20,),
-
-        Container(
-          padding: EdgeInsets.all(10), // 수정된 부분
-          width: 350,
-          height: 161,
-          margin: EdgeInsets.symmetric(horizontal: 20), // 수정된 부분
-          child: Column(
-            children: [
-              Container( // 키워드 별 컨테이너 한 개
-                padding: EdgeInsets.all(10),
-                height: 130,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Row( // 키워드 제목
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          child: Row(
-                            children: [
-                              Icon(Icons.computer, color: Colors.black,),
-                              SizedBox(width: 10,),
-                              Text('IT/과학', style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 23,
-                                fontFamily: 'NanumSquareRoundEB',
-                                fontWeight: FontWeight.w500
-                              ),),
-                            ],
-                          ),
-                        ),
-                        Text('업데이트 시간') // 추후 업데이트 시간으로 변경
-                      ],
-                    ),
-                    Container( // 기사 목록
-                      child: Column(
-                      children: [
-                        Row( // 뉴스 기사 1
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('기사 제목 줄여서 출력하기 위한 예시...'),
-                            Text('언론사')
-                          ],
-                        ),
-                        Row( // 뉴스 기사 2
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('기사 제목 줄여서 출력하기 위한 예시...'),
-                            Text('언론사')
-                          ],
-                        ),
-                        Row( // 뉴스 기사 3
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('기사 제목 줄여서 출력하기 위한 예시...'),
-                            Text('언론사')
-                          ],
-                        ),
-                      ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(14),
-            boxShadow: [
-              BoxShadow(
-                color: Color(0x29000000),
-                offset: Offset(0, 1),
-                blurRadius: 6,
-                spreadRadius: 0,
-              ),
-            ],
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
