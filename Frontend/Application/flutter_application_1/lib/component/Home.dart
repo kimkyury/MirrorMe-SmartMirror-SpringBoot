9c25711a019bcec78d2f1b1d2422fe847d277370
이소정 <stuj7913@naver.com>
2023-08-18 08:58:10 +0900

    chore : temp
    Former-commit-id: 1cb1d966cde6e9c3a58b370baf43e5e39ba130f7 [formerly a68c69b234bfd29e7a6b2b301a67fce43396cdc7]
    Former-commit-id: efefaf2846637c2578a062197e67bd9d5fa6c27e

--- Frontend/Application/flutter_application_1/lib/component/Home.dart
+++ Frontend/Application/flutter_application_1/lib/component/Home.dart
@@ -50,7 +50,7 @@ class Home extends StatelessWidget {
                   indent: 15, // 선 좌측 여백
                   endIndent: 15, // 선 우측 여백
                 ),
-                WeekWeather(), // 주간 날씨
+                // WeekWeather(), // 주간 날씨
                 SizedBox(height: 10,), ////////////////////// 상세 조정 필요////////////////////////
               ],
             ),
