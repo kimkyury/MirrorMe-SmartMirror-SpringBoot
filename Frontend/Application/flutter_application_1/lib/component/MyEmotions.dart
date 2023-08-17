2ea2a143a5e9e897cb5e46993e04ecce19edb47c
이소정 <stuj7913@naver.com>
2023-08-17 21:32:48 +0900

    feat : application my emotion
    Former-commit-id: 47824dfc84273b003b6aab7c5ca801f41f0a2295 [formerly 1d30a05f9899065c00fe1558dd1e88ede50a35ab]
    Former-commit-id: 349394f56bf36a5e72285199417758a3a23a5942

--- Frontend/Application/flutter_application_1/lib/component/MyEmotions.dart
+++ Frontend/Application/flutter_application_1/lib/component/MyEmotions.dart
@@ -11,7 +11,7 @@ void main() {
 }
 
 class MyEmotions extends StatelessWidget {
-  const MyEmotions({Key? key});
+  const MyEmotions({Key? key});  
 
   @override
   Widget build(BuildContext context) {
